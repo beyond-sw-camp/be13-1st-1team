ddb4d1f1b63d735d7809d7d92c8fcdbf6b812b5b
jelee55 <jegyeong25@gmail.com>
2024-12-30 12:51:50 +0900

    판매자 관련 삭제 및 오타 수정

--- sql/order.sql
+++ sql/order.sql
@@ -10,7 +10,7 @@ END $$
 
 DELIMITER ;
 
--- 주문 등록판매자가 주문을 진행 상태로 등록할 수 있다.	
+-- 주문 소비자가 주문을 진행 상태로 등록할 수 있다.	
 DELIMITER $$
 
 CREATE PROCEDURE CreateOrder(IN p_userId INT, IN p_packageId INT)
@@ -22,24 +22,8 @@ END $$
 DELIMITER ;
 
 
--- 주문(주문자-대기) 수정	주문자가 대기 상태인 주문을 수정할 수 있다.	
-DELIMITER $$
 
-CREATE PROCEDURE UpdateOrderStatusToWaiting(IN p_orderId INT, IN p_userId INT)
-BEGIN
-    IF EXISTS (SELECT 1 FROM `order` WHERE `id` = p_orderId AND `userId` = p_userId AND `status` = 0) THEN
-        UPDATE `order` 
-        SET `status` = 0, `updatedAt` = NOW() 
-        WHERE `id` = p_orderId;
-    ELSE
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '주문 상태가 대기가 아닙니다.';
-    END IF;
-END $$
-
-DELIMITER ;
-		
-
--- 주문(주문자-진행) 수정	주문자가 진행 상태인 주문을 수정할 수 있다.			
+-- 수정 주문자가 진행 상태인 주문을 수정할 수 있다.			
 DELIMITER $$
 
 CREATE PROCEDURE UpdateOrderStatusToInProgress(IN p_orderId INT, IN p_userId INT)
@@ -56,7 +40,7 @@ END $$
 DELIMITER ;
 
 
--- 주문(주문자-확정) 수정	주문자가 확정 상태인 주문을 수정할 수 없다.			
+--  주문자가 확정 상태인 주문을 수정할 수 없다.			
 DELIMITER $$
 
 CREATE PROCEDURE UpdateOrderStatusToConfirmed(IN p_orderId INT, IN p_userId INT)
@@ -71,51 +55,6 @@ END $$
 DELIMITER ;
 
 
--- 주문(판매자-대기) 수정		판매자가 등록 상태인 주문을 수정할 수 있다.			
-DELIMITER $$
-
-CREATE PROCEDURE SellerUpdateOrderStatusToWaiting(IN p_orderId INT, IN p_userId INT)
-BEGIN
-    IF EXISTS (SELECT 1 FROM `order` WHERE `id` = p_orderId AND `userId` = p_userId AND `status` = 0) THEN
-        UPDATE `order` 
-        SET `status` = 0, `updatedAt` = NOW() 
-        WHERE `id` = p_orderId;
-    ELSE
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '주문 상태가 대기가 아닙니다.';
-    END IF;
-END $$
-
-DELIMITER ;
-
-
--- 주문(판매자-진행) 수정	판매자가 진행 상태인 주문을 수정할 수 없다.			
-DELIMITER $$
-
-CREATE PROCEDURE SellerUpdateOrderStatusToInProgress(IN p_orderId INT, IN p_userId INT)
-BEGIN
-    IF EXISTS (SELECT 1 FROM `order` WHERE `id` = p_orderId AND `userId` = p_userId AND `status` = 1) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '진행 상태의 주문은 수정할 수 없습니다.';
-    ELSE
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '주문 상태가 진행이 아닙니다.';
-    END IF;
-END $$
-
-DELIMITER ;
-
-
--- 주문(판매자-확정) 수정 판매자가 확정 상태인 주문을 수정할 수 없다.			
-DELIMITER $$
-
-CREATE PROCEDURE SellerUpdateOrderStatusToConfirmed(IN p_orderId INT, IN p_userId INT)
-BEGIN
-    IF EXISTS (SELECT 1 FROM `order` WHERE `id` = p_orderId AND `userId` = p_userId AND `status` = 2) THEN
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '확정 상태의 주문은 수정할 수 없습니다.';
-    ELSE
-        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '주문 상태가 확정이 아닙니다.';
-    END IF;
-END $$
-
-DELIMITER ;
 
 
 -- 주문 확정	판매자가 진행 상태인 주문을 확정할 수 있다.			
